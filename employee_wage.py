ad209e3239b0ec170162ea75e5fc63e9aa82d7e7
naveen <naiknaveen940@gmail.com>
2024-07-31 18:25:56 +0530

    [Naveen] modify:Getting employee wage using Switch Case Statement

--- employee_wage.py
+++ employee_wage.py
@@ -4,7 +4,7 @@
 @Date: 2024-07-31 15:20:00
 @Last Modified by: Naveen Madev Naik
 @Last Modified time: 2024-07-31 
-@Title :Add Part time Employee & Wage
+@Title :Getting employee wage using Switch Case Statement
 
 
 '''
@@ -45,15 +45,15 @@ def check_attendence():
         None
 
     Return:
-        None  
+        Boolean 
     """
 
 
     random_value=random.randint(0,1)
     if random_value==0:
-        return 0
+        return False
     else:
-        return 1
+        return True
     
 
 def calculate_employee_wage(wage_per_hour,work_hour):
@@ -77,20 +77,28 @@ def calculate_employee_wage(wage_per_hour,work_hour):
 def main():
     try:
         print("Part time employee wage details:")
-        if check_attendence()==1:
-            print(f"Employee is Present")
-            calculate_employee_wage(WAGE_PER_HOUR,PART_TIME_HOUR)
-        else:
-            print(f"Employee is Absent") 
-            print(f"employee wage is: {0}$") 
+        checkAttendence=check_attendence()
+        match checkAttendence:
+            case True:
+                print(f"Employee is Present")
+                calculate_employee_wage(WAGE_PER_HOUR,PART_TIME_HOUR)
+            case False:
+                print(f"Employee is Absent") 
+                print(f"employee wage is: {0}$") 
+  
+
 
         print("\nFull time employee wage details:")
-        if check_attendence()==1:
-            print(f"Employee is Present")
-            calculate_employee_wage(WAGE_PER_HOUR,FULL_DAY_HOUR)
-        else:
-            print(f"Employee is Absent") 
-            print(f"employee wage is: {0}$")    
+        full_time_check_attendence=check_attendence()
+        match full_time_check_attendence:
+            case 1:
+                print(f"Employee is Present")
+                calculate_employee_wage(WAGE_PER_HOUR,FULL_DAY_HOUR)
+            case 0:
+                print(f"Employee is Absent") 
+                print(f"employee wage is: {0}$") 
+            case _:
+                print("enter valid input")     
 
     except Exception as e:
         print(e)    
