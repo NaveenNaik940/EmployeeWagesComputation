e3ef6146bc3db4118752ef9db09d09eb5f560e9f
naveen <naiknaveen940@gmail.com>
2024-07-31 19:07:05 +0530

    [Naveen] modify:calculating employee wages for month

--- employee_wage.py
+++ employee_wage.py
@@ -4,7 +4,7 @@
 @Date: 2024-07-31 15:20:00
 @Last Modified by: Naveen Madev Naik
 @Last Modified time: 2024-07-31 
-@Title :Getting employee wage using Switch Case Statement
+@Title :Calculating employee Wages for a Month(20days)
 
 
 '''
@@ -13,8 +13,9 @@
 import random
 
 WAGE_PER_HOUR=20
-FULL_DAY_HOUR=8
+FULL_TIME_HOUR=8
 PART_TIME_HOUR=4
+WORKING_DAY=20
 
 def display_welcome_message():
 
@@ -56,7 +57,7 @@ def check_attendence():
         return True
     
 
-def calculate_employee_wage(wage_per_hour,work_hour):
+def calculate_employee_wage(wage_per_hour,work_hour,working_day):
 
 
     """
@@ -66,39 +67,37 @@ def calculate_employee_wage(wage_per_hour,work_hour):
     Parameter:
         wage_per_hour:employee's wage per hour
         work_hour:employee's working hour
+        working_day:employee's working day
 
     Return:
         None
     """
 
-    print(f"employee wage is: {wage_per_hour*work_hour}$")
+    print(f"employee wage is: {wage_per_hour*work_hour*working_day}$\n")
 
 
 def main():
     try:
-        print("Part time employee wage details:")
-        checkAttendence=check_attendence()
-        match checkAttendence:
-            case True:
-                print(f"Employee is Present")
-                calculate_employee_wage(WAGE_PER_HOUR,PART_TIME_HOUR)
-            case False:
-                print(f"Employee is Absent") 
-                print(f"employee wage is: {0}$") 
-  
-
-
-        print("\nFull time employee wage details:")
-        full_time_check_attendence=check_attendence()
-        match full_time_check_attendence:
-            case 1:
-                print(f"Employee is Present")
-                calculate_employee_wage(WAGE_PER_HOUR,FULL_DAY_HOUR)
-            case 0:
-                print(f"Employee is Absent") 
-                print(f"employee wage is: {0}$") 
-            case _:
-                print("enter valid input")     
+        no_of_employee=int(input("enter number of employees you want to check their wages: "))
+        if no_of_employee<1:
+            raise ValueError("Enter positive integer greater than 0")   
+        for index in range(no_of_employee):
+            print(f"enter employee {index+1} type: full time(f,F) or part time(p,P)")
+            employee_type=input("F,f/p,P: ")[0]
+            if employee_type not in "fFpP":
+                raise ValueError("enter proper employee type")
+            if employee_type=="p" or employee_type=='P':
+                if check_attendence():
+                    print(f"Employee {index+1} Present and his wage:")
+                    calculate_employee_wage(WAGE_PER_HOUR,PART_TIME_HOUR,WORKING_DAY)
+                else:    
+                    print(f"Employee {index+1}is Absent and his wage: {0}$")
+            else:
+                if check_attendence():
+                    print(f"Employee {index+1} Present and his wage:")
+                    calculate_employee_wage(WAGE_PER_HOUR,FULL_TIME_HOUR,WORKING_DAY)
+                else:    
+                    print(f"Employee {index+1}is Absent and his wage: {0}$")    
 
     except Exception as e:
         print(e)    
