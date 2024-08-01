58621ae0c8de8742cceeb089f1da83fac1ea087b
naveen <naiknaveen940@gmail.com>
2024-08-01 12:46:22 +0530

    [Naveen] modify:calculating wage till 100 hours and 20 days working day

--- employee_wage.py
+++ employee_wage.py
@@ -1,10 +1,10 @@
 '''
 
 @Author:Naveen Madev Naik
-@Date: 2024-07-31 15:20:00
+@Date: 2024-08-01 15:20:00
 @Last Modified by: Naveen Madev Naik
-@Last Modified time: 2024-07-31 
-@Title :Calculating employee Wages for a Month(20days)
+@Last Modified time: 2024-08-01 
+@Title :Calculate Employee Wages till a condition of total working hours or days is reached for a month
 
 
 '''
@@ -16,6 +16,7 @@ WAGE_PER_HOUR=20
 FULL_TIME_HOUR=8
 PART_TIME_HOUR=4
 WORKING_DAY=20
+TOTAL_WORKING_HOUR=100
 
 def display_welcome_message():
 
@@ -31,11 +32,26 @@ def display_welcome_message():
         None        
     """
 
-
     print("Hello Everyone welcome to Employee Wage Computation")
 
 
-def check_attendence():
+def is_part_time_or_full_time():
+    """
+    Description:
+        The function wil return part time or full time
+
+    Parameter:
+        None
+
+    Return:
+        string    
+    """
+
+    employee_type=random.choice(["Part_time","Full_time"])
+    return employee_type
+
+
+def is_present():
 
 
     """
@@ -57,47 +73,51 @@ def check_attendence():
         return True
     
 
-def calculate_employee_wage(wage_per_hour,work_hour,working_day):
+def employee_daily_wage(wage_per_hour,work_hour):
 
 
     """
     Description:
-        function will return the employee wage 
+        function will return the employee wage per day 
 
     Parameter:
         wage_per_hour:employee's wage per hour
         work_hour:employee's working hour
-        working_day:employee's working day
 
     Return:
-        None
+        int
     """
 
-    print(f"employee wage is: {wage_per_hour*work_hour*working_day}$\n")
+    return (int)(work_hour*wage_per_hour)
 
 
 def main():
     try:
-        no_of_employee=int(input("enter number of employees you want to check their wages: "))
-        if no_of_employee<1:
-            raise ValueError("Enter positive integer greater than 0")   
-        for index in range(no_of_employee):
-            print(f"enter employee {index+1} type: full time(f,F) or part time(p,P)")
-            employee_type=input("F,f/p,P: ")[0]
-            if employee_type not in "fFpP":
-                raise ValueError("enter proper employee type")
-            if employee_type=="p" or employee_type=='P':
-                if check_attendence():
-                    print(f"Employee {index+1} Present and his wage:")
-                    calculate_employee_wage(WAGE_PER_HOUR,PART_TIME_HOUR,WORKING_DAY)
-                else:    
-                    print(f"Employee {index+1}is Absent and his wage: {0}$")
+        print("Employee Wage for the month: ")
+
+        work_day=1
+        work_hour=0
+        wage_list=[]
+        total_wage=0
+        while work_hour<=TOTAL_WORKING_HOUR and  work_day<=WORKING_DAY:
+            if is_present():
+                if is_part_time_or_full_time=="Full_time":
+                    work_hour+=FULL_TIME_HOUR
+                    wage_list.append(employee_daily_wage(WAGE_PER_HOUR,FULL_TIME_HOUR))
+                    total_wage+=employee_daily_wage(WAGE_PER_HOUR,FULL_TIME_HOUR)
+                else:
+                     work_hour+=PART_TIME_HOUR   
+                     wage_list.append(employee_daily_wage(WAGE_PER_HOUR,PART_TIME_HOUR))
+                     total_wage+=employee_daily_wage(WAGE_PER_HOUR,PART_TIME_HOUR)
+                work_day+=1     
+
             else:
-                if check_attendence():
-                    print(f"Employee {index+1} Present and his wage:")
-                    calculate_employee_wage(WAGE_PER_HOUR,FULL_TIME_HOUR,WORKING_DAY)
-                else:    
-                    print(f"Employee {index+1}is Absent and his wage: {0}$")    
+                wage_list.append(0)
+                work_day+=1
+
+        print("wage list of Employee($):") 
+        print(wage_list) 
+        print(f"total wage: {total_wage}$")      
 
     except Exception as e:
         print(e)    
