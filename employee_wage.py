170e8a6418442b05944693cec069b5e6155dd5ef
naveen <naiknaveen940@gmail.com>
2024-08-02 10:36:53 +0530

    [Naveen] modify:using class methods

--- employee_wage.py
+++ employee_wage.py
@@ -4,125 +4,143 @@
 @Date: 2024-08-01 15:20:00
 @Last Modified by: Naveen Madev Naik
 @Last Modified time: 2024-08-01 
-@Title :Calculate Employee Wages till a condition of total working hours or days is reached for a month
-
+@Title :Refactoring the Code to write a Class Method to Compute Employee Wage
 
 '''
 
 
 import random
 
-WAGE_PER_HOUR=20
-FULL_TIME_HOUR=8
-PART_TIME_HOUR=4
-WORKING_DAY=20
-TOTAL_WORKING_HOUR=100
+class EmployeeWage:
+    WAGE_PER_HOUR = 20
+    FULL_TIME_HOUR = 8
+    PART_TIME_HOUR = 4
+    WORKING_DAY = 20
+    TOTAL_WORKING_HOUR = 100
+     
+    @staticmethod
+    def display_welcome_message(cls):
+        
+        
+        """
+        Description:
+            Will print the welcome message.
 
-def display_welcome_message():
+        Parameter:
+            cls: Represents the class itself.
 
+        Return:
+            None        
+        """
+        
+        
+        print("Hello Everyone welcome to Employee Wage Computation")
 
-    """
-    Description:
-        will print the welcome message
+    @classmethod
+    def is_part_time_or_full_time(cls):
+        
+        
+        """
+        Description:
+            The function will return part time or full time.
 
-    Paramater:
-        None
+        Parameter:
+            cls: Represents the class itself.
 
-    Return:
-        None        
-    """
+        Return:
+            string    
+        """
+        
+        
+        return random.choice(["Part_time", "Full_time"])
 
-    print("Hello Everyone welcome to Employee Wage Computation")
+    @classmethod
+    def is_present(cls):
+        
+        
+        """
+        Description:
+            Function return Absent or present based on random value generated.
 
+        Parameter:
+            cls: Represents the class itself.
 
-def is_part_time_or_full_time():
-    """
-    Description:
-        The function wil return part time or full time
+        Return:
+            Boolean 
+        """
+        
+        
+        return random.choice([True, False])
 
-    Parameter:
-        None
+    @classmethod
+    def employee_daily_wage(cls, wage_per_hour, work_hour):
+        
+        
+        """
+        Description:
+            Function will return the employee wage per day.
 
-    Return:
-        string    
-    """
+        Parameter:
+            cls: Represents the class itself.
+            wage_per_hour: employee's wage per hour
+            work_hour: employee's working hour
 
-    employee_type=random.choice(["Part_time","Full_time"])
-    return employee_type
+        Return:
+            int
+        """
+        
+        
+        return work_hour * wage_per_hour
+    @classmethod
+    def compute_wages(cls):
+        
+        
+        """
+        Description:
+            Computes the wages for the employee for a month.
 
+        Parameter:
+            cls: Represents the class itself.
 
-def is_present():
+        Return:
+            None
+        """
+        
+        
+        try:
+            work_day = 1
+            work_hour = 0
+            wage_list = []
+            total_wage = 0
+            print("Employee Wage for the month:")
 
+            while work_hour <= cls.TOTAL_WORKING_HOUR and work_day <= cls.WORKING_DAY:
+                if EmployeeWage.is_present():
+                    if EmployeeWage.is_part_time_or_full_time() == "Full_time":
+                        work_hour += cls.FULL_TIME_HOUR
+                        daily_wage = EmployeeWage.employee_daily_wage(cls.WAGE_PER_HOUR, cls.FULL_TIME_HOUR)
+                    else:
+                        work_hour += cls.PART_TIME_HOUR
+                        daily_wage = EmployeeWage.employee_daily_wage(cls.WAGE_PER_HOUR, cls.PART_TIME_HOUR)
 
-    """
-    Description:
-        function return Absent or present based on random value generated
+                    wage_list.append(daily_wage)
+                    total_wage += daily_wage
+                else:
+                    wage_list.append(0)
+                
+                work_day += 1
 
-    Parameter:
-        None
+            print("Wage list of Employee ($):")
+            print(wage_list)
+            print(f"Total wage: {total_wage}$")
 
-    Return:
-        Boolean 
-    """
-
-
-    random_value=random.randint(0,1)
-    if random_value==0:
-        return False
-    else:
-        return True
-    
-
-def employee_daily_wage(wage_per_hour,work_hour):
-
-
-    """
-    Description:
-        function will return the employee wage per day 
-
-    Parameter:
-        wage_per_hour:employee's wage per hour
-        work_hour:employee's working hour
-
-    Return:
-        int
-    """
-
-    return (int)(work_hour*wage_per_hour)
-
+        except Exception as e:
+            print(e)
 
 def main():
-    try:
-        print("Employee Wage for the month: ")
-
-        work_day=1
-        work_hour=0
-        wage_list=[]
-        total_wage=0
-        while work_hour<=TOTAL_WORKING_HOUR and  work_day<=WORKING_DAY:
-            if is_present():
-                if is_part_time_or_full_time()=="Full_time":
-                    work_hour+=FULL_TIME_HOUR
-                    wage_list.append(employee_daily_wage(WAGE_PER_HOUR,FULL_TIME_HOUR))
-                    total_wage+=employee_daily_wage(WAGE_PER_HOUR,FULL_TIME_HOUR)
-                else:
-                     work_hour+=PART_TIME_HOUR   
-                     wage_list.append(employee_daily_wage(WAGE_PER_HOUR,PART_TIME_HOUR))
-                     total_wage+=employee_daily_wage(WAGE_PER_HOUR,PART_TIME_HOUR)
-                work_day+=1     
-
-            else:
-                wage_list.append(0)
-                work_day+=1
-
-        print("wage list of Employee($):") 
-        print(wage_list) 
-        print(f"total wage: {total_wage}$")      
-
-    except Exception as e:
-        print(e)    
+    EmployeeWage.display_welcome_message()
+    EmployeeWage.compute_wages()
 
 
-if __name__=="__main__":
-    display_welcome_message()
+if __name__ == "__main__":
     main()
