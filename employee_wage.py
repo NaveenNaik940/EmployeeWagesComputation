e4a8f4f18c55fb014774b31609808323e1f458c8
naveen <naiknaveen940@gmail.com>
2024-08-03 15:48:15 +0530

    [Naveen] modify:modified the code according to uc9

--- employee_wage.py
+++ employee_wage.py
@@ -4,7 +4,7 @@
 @Date: 2024-08-02 15:20:00
 @Last Modified by: Naveen Madev Naik
 @Last Modified time: 2024-08-02 
-@Title :Compute Employee Wage for multiple companies
+@Title :Compute Employee Wage for multiple companies and ability to save the total Wage for each company
 
 '''
 
@@ -19,6 +19,50 @@ class EmployeeWage:
         self.part_time_hour = part_time_hour
         self.working_day = working_day
         self.total_working_hour = total_working_hour
+
+
+    def compute_wages(self):
+        
+        
+        """
+        Description:
+            Computes the wages for the employee for a month.
+
+        Parameter:
+            self: Represents the instance of class.
+
+        Return:
+            dictionary
+        """
+        
+        
+        try:
+            work_day = 1
+            work_hour = 0
+            wage_list = []
+            total_wage = 0
+            wages_dict={}
+
+            while work_hour <= self.total_working_hour and work_day <= self.working_day:
+                if EmployeeWage.is_present():
+                    if EmployeeWage.is_part_time_or_full_time() == "Full_time":
+                        work_hour += self.full_time_hour
+                        daily_wage = EmployeeWage.employee_daily_wage(self.wage_per_hour, self.full_time_hour)
+                    else:
+                        work_hour += self.part_time_hour
+                        daily_wage = EmployeeWage.employee_daily_wage(self.wage_per_hour, self.part_time_hour)
+
+                    wage_list.append(daily_wage)
+                    total_wage += daily_wage
+                else:
+                    wage_list.append(0)
+                
+                work_day += 1
+            
+            wages_dict={"wage_list":wage_list,"total_wage":total_wage}
+            return wages_dict
+        except Exception as e:
+            print(e)
 
 
     @staticmethod
@@ -39,8 +83,8 @@ class EmployeeWage:
         print("Hello Everyone welcome to Employee Wage Computation")
 
 
-    @classmethod
-    def is_part_time_or_full_time(cls):
+    @staticmethod
+    def is_part_time_or_full_time():
         
         
         """
@@ -48,17 +92,17 @@ class EmployeeWage:
             The function will return part time or full time.
 
         Parameter:
-            cls: Represents the class itself.
+            None.
 
         Return:
-            string    
+            str:Part_time or Full_time    
         """
         
         
         return random.choice(["Part_time", "Full_time"])
 
-    @classmethod
-    def is_present(cls):
+    @staticmethod
+    def is_present():
         
         
         """
@@ -66,7 +110,7 @@ class EmployeeWage:
             Function return Absent or present based on random value generated.
 
         Parameter:
-            cls: Represents the class itself.
+            None
 
         Return:
             Boolean 
@@ -95,71 +139,123 @@ class EmployeeWage:
         
         return work_hour * wage_per_hour
     
-    def compute_wages(self):
-        
-        
+
+
+
+class EmpWageBuilder:
+    def __init__(self):
+        self.company_wages = {}
+
+    def add_company(self, company_name, wage_per_hour, full_time_hour, part_time_hour, working_day, total_working_hour):
+
+
         """
         Description:
-            Computes the wages for the employee for a month.
+            Adds a company to the wage builder.
 
-        Parameter:
-            self: Represents the instance of class.
+        Parameters:
+            company_name (str): The name of the company
+            wage_per_hour (int): Wage per hour for the company
+            full_time_hour (int): Full-time hours for the company
+            part_time_hour (int): Part-time hours for the company
+            working_day (int): Number of working days for the company
+            total_working_hour (int): Total working hours for the company
 
         Return:
-            dictionary
+            None
         """
-        
-        
-        try:
-            work_day = 1
-            work_hour = 0
-            wage_list = []
-            total_wage = 0
-            wages_dict={}
 
-            while work_hour <= self.total_working_hour and work_day <= self.working_day:
-                if self.is_present():
-                    if EmployeeWage.is_part_time_or_full_time() == "Full_time":
-                        work_hour += self.full_time_hour
-                        daily_wage = EmployeeWage.employee_daily_wage(self.wage_per_hour, self.full_time_hour)
-                    else:
-                        work_hour += self.part_time_hour
-                        daily_wage = EmployeeWage.employee_daily_wage(self.wage_per_hour, self.part_time_hour)
 
-                    wage_list.append(daily_wage)
-                    total_wage += daily_wage
-                else:
-                    wage_list.append(0)
-                
-                work_day += 1
-            
-            wages_dict={"wage_list":wage_list,"total_wage":total_wage}
-            return wages_dict
-        except Exception as e:
-            print(e)
-    
+        employee_wage = EmployeeWage(
+            company_name=company_name,
+            wage_per_hour=wage_per_hour,
+            full_time_hour=full_time_hour,
+            part_time_hour=part_time_hour,
+            working_day=working_day,
+            total_working_hour=total_working_hour
+        )
+        self.company_wages[company_name] = employee_wage
+
+    def compute_wages(self):
+
+
+        """
+        Description:
+            Computes the wages for all added companies.
+
+        Parameter:
+            None
+
+        Return:
+            None
+        """
+
+        
+        for company, employee_wage in self.company_wages.items():
+            wages = employee_wage.compute_wages()
+            print(f"\nComputing wages for {company}:")
+            print(f"Employee wages list in {company}: ")
+            print(wages['wage_list'])
+            print(f"Total wage: {wages['total_wage']}$")
+
+
+class EmpWageBuilder:
+
+    def __init__(self):
+        self.company_wages = {}
+
+    def add_company(self, company_name, wage_per_hour, full_time_hour, part_time_hour, working_day, total_working_hour):
+        """
+        Description:
+            Adds a company to the wage builder.
+
+        Parameters:
+            company_name (str): The name of the company
+            wage_per_hour (int): Wage per hour for the company
+            full_time_hour (int): Full-time hours for the company
+            part_time_hour (int): Part-time hours for the company
+            working_day (int): Number of working days for the company
+            total_working_hour (int): Total working hours for the company
+
+        Return:
+            None
+        """
+        employee_wage = EmployeeWage(
+            wage_per_hour=wage_per_hour,
+            full_time_hour=full_time_hour,
+            part_time_hour=part_time_hour,
+            working_day=working_day,
+            total_working_hour=total_working_hour
+        )
+        self.company_wages[company_name] = employee_wage
+
+    def compute_wages(self):
+        """
+        Description:
+            Computes the wages for all added companies.
+
+        Parameter:
+            None
+
+        Return:
+            None
+        """
+        for company, employee_wage in self.company_wages.items():
+            wages = employee_wage.compute_wages()
+            print(f"\nComputing wages for {company}:")
+            print(f"Employee wages list in {company}: ")
+            print(wages['wage_list'])
+            print(f"Total wage: {wages['total_wage']}$")
 
 def main():
     EmployeeWage.display_welcome_message()
-    companies = [
-        {"name": "BridgeLabz", "wage_per_hour": 15, "full_time_hour": 8, "part_time_hour": 4, "working_days": 24, "total_working_hours": 100},
-        {"name": "Truemind", "wage_per_hour": 22, "full_time_hour": 8, "part_time_hour": 4, "working_days": 22, "total_working_hours": 110},
-        {"name": "Accenture", "wage_per_hour": 18, "full_time_hour": 8, "part_time_hour": 4, "working_days": 25, "total_working_hours": 120}
-    ]
-    wage_dict={}
-    for company in companies:
-        print(f"\nComputing wages for {company['name']}:")
-        employee_wage = EmployeeWage(
-            wage_per_hour=company["wage_per_hour"],
-            full_time_hour=company["full_time_hour"],
-            part_time_hour=company["part_time_hour"],
-            working_day=company["working_days"],
-            total_working_hour=company["total_working_hours"]
-        )
-        wage_dict[company['name']]=employee_wage.compute_wages()
-        print(f"Employee wages list in {company['name']}: ")
-        print(wage_dict[company['name']]['wage_list'])
-        print(f"total wage: {wage_dict[company['name']]['total_wage']}$")
+    
+    emp_wage_builder = EmpWageBuilder()
+    emp_wage_builder.add_company("BridgeLabz", 15, 8, 4, 24, 100)
+    emp_wage_builder.add_company("Truemind", 22, 8, 4, 22, 110)
+    emp_wage_builder.add_company("Accenture", 18, 8, 4, 25, 120)
+    
+    emp_wage_builder.compute_wages()
       
 
 
